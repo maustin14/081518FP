60ff229377687acec4ef79204ab708cea86dddf0
Montrell Austin <montrell.austin@gmail.com>
2018-08-16 22:28:45 +0000

    Comfirmation on logout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,15 @@ class PostsController < ApplicationController
 
   def index
   end
+    
+  def create
+  @post = Post.new(params[:post].permit(:title, :text))
 
+   @post.save
+   redirect_to @post
+ end
+  
+    
   def show
   end
 end
